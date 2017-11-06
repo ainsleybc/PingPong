6841ba193a1117363883449b2eb82de4bd88e895
Canace Wong <canacewongtoday@hotmail.com>
2017-11-06 17:42:49 +0000

    notepad list test pass

--- spec/notepadSpec.js
+++ spec/notepadSpec.js
@@ -1,8 +1,9 @@
 var Notepad = require('../src/notepad.js');
+
 (function(exports) {
   (function() {
     var notepad = new Notepad();
-    if (false) throw new Error('No list of notes!');
+    if (notepad.notes().length !== 0) throw new Error('No list of notes!');
     console.log('Test passing!');
   })();
 })(this);
