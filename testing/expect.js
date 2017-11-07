5e4888b596f635ba4165e8993d24c114b2c9222a
ainsleybc <ab.chang@me.com>
2017-11-07 16:48:41 +0000

    refactored test library to Dry out error messages

--- testing/expect.js
+++ testing/expect.js
@@ -4,43 +4,60 @@ var expect = function(firstArg) {
 
   function testPassing() {
     console.log('The test passes!');
-  }
+  };
 
   function executeTest(options) {
+    var assertion = options.assertion;
+    var errorOperator = options.errorOperator;
+    var errorType = options.errorType;
+    var secondArg = options.secondArg
+
     if (options.assertion) {
       testPassing();
     } else {
-      throw new Error(options.errorMessage);
+      throw new Error(errorMessage(errorType, errorOperator, secondArg));
     }
-  }
+  };
+
+  function errorMessage(errorType, errorOperator, secondArg) {
+    if (errorType === 'single comparison') {
+      return "Test failed: " + firstArg + " is not true.";
+    } else {
+      return 'Test failed: expected ' + firstArg + errorOperator + secondArg + '.';        
+    }
+  };
 
   return {
 
     toBeTrue: function() {
       executeTest({
         assertion: (!firstArg),
-        errorMessage: ("Test failed: " + firstArg + " is not true.")
+        errorOperator: (" is not true"),
+        errorType: 'single comparison'
       });
     },
 
     toEqual: function(secondArg) {
       executeTest({
         assertion: (firstArg === secondArg),
-        errorMessage: ("Test failed: expected " + firstArg + " to equal " + secondArg + '.')
+        errorOperator: (" to equal "),
+        secondArg: secondArg
       });
     },
 
     toNotEqual: function(secondArg) {
       executeTest({
         assertion: (firstArg !== secondArg),
-        errorMessage: ("Test failed: expected " + firstArg + " not to equal " + secondArg + '.')
+        errorOperator: (" not to equal "),
+        secondArg: secondArg        
       });
     },
 
     toInclude: function(secondArg) {
       executeTest({
         assertion: (firstArg.includes(secondArg)),
-        errorMessage: ("Test failed: expected " + firstArg + " to contain " + secondArg + ".")
+        errorOperator: (" to contain "),
+        secondArg: secondArg        
       });
     }
   };
