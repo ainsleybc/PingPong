8554128ff9bbb38534f35168e40157e52966109a
hugosykes <hugo@denmeadfarm.co.uk>
2017-11-07 11:46:40 +0000

    Added the addNote functionality to the notepad

--- spec/notepadSpec.js
+++ spec/notepadSpec.js
@@ -1,9 +1,21 @@
 var Notepad = require('../src/notepad.js');
 var assert = require('../testing/assert.js');
+var noteSpy = {
+  contents: 'contents'
+};
 
 (function(exports) {
   (function() {
     var notepad = new Notepad();
     assert.isThis(notepad.notes().length).equalTo(0);
+  })();
+})(this);
+
+(function(exports) {
+  (function() {
+    var notepad = new Notepad();
+    notepad.addNote(noteSpy);
+    assert.isThis(notepad.notes().length).equalTo(1);
+    assert.isThis(notepad.notes()[0].contents).equalTo('contents');
   })();
 })(this);
--- src/notepad.js
+++ src/notepad.js
@@ -1,8 +1,10 @@
 function Notepad() {
-  var listOfNotes = [];
-
-  Notepad.prototype.notes = function() {
-    return listOfNotes;
-  };
+  this._listOfNotes = [];
 }
+Notepad.prototype.addNote = function(note) {
+  this._listOfNotes.push(note);
+};
+Notepad.prototype.notes = function() {
+  return this._listOfNotes;
+};
 module.exports = Notepad;
