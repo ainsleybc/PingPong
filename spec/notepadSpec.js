88fe831993a65aca4424817c9650c73ba5fa0b85
hugosykes <hugo@denmeadfarm.co.uk>
2017-11-07 17:33:59 +0000

    Added the listHTML function to the notepad

--- spec/notepadSpec.js
+++ spec/notepadSpec.js
@@ -19,3 +19,25 @@ var noteSpy = {
     expect(notepad.notes()[0].contents).toEqual('contents');
   })();
 })(this);
+
+(function(exports) {
+  (function() {
+    var notepad = new Notepad();
+    notepad.addNote({
+      abbreviation: 'hello'
+    });
+    notepad.addNote({
+      abbreviation: 'my'
+    });
+    notepad.addNote({
+      abbreviation: 'name'
+    });
+    notepad.addNote({
+      abbreviation: 'is'
+    });
+    notepad.addNote({
+      abbreviation: 'Canace'
+    });
+    expect(notepad.listHTML()).toEqual('<ul><li>hello</li><li>my</li><li>name</li><li>is</li><li>Canace</li></ul>');
+  })();
+})(this);
--- src/notepad.js
+++ src/notepad.js
@@ -9,3 +9,10 @@ Notepad.prototype.addNote = function(note) {
 Notepad.prototype.notes = function() {
   return this._listOfNotes;
 };
+Notepad.prototype.listHTML = function() {
+  var html = '<ul>';
+  for (var i = 0; i < this._listOfNotes.length; i++) {
+    html += ('<li>' + this.notes()[i].abbreviation + '</li>');
+  }
+  return html + '</ul>';
+};
