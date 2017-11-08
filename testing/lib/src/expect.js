e7c73703d3c64462a65e5493a8c33df15de645e8
ainsleybc <ab.chang@me.com>
2017-11-08 15:52:42 +0000

    added execution context switching for assertions

--- testing/lib/src/expect.js
+++ testing/lib/src/expect.js
@@ -4,13 +4,15 @@
   
   exports.expect = function (firstArg) {
 
-    function executeTest(options) {
+    const not = ' not';
+
+    function _executeTest(options) {
       if (!options.assertion) {
-        throw new Error(errorMessage(options.errorType, options.errorOperator, options.secondArg));
+        throw new Error(_errorMessage(options.errorType, options.errorOperator, options.secondArg));
       };
     };
 
-    function errorMessage(errorType, errorOperator, secondArg) {
+    function _errorMessage(errorType, errorOperator, secondArg) {
       if (errorType === 'single comparison') {
         return "Test failed: " + firstArg + " is not true.";
       } else {
@@ -18,51 +20,65 @@
       }
     };
 
-    function toEqual (secondArg) {
-      executeTest({
-        assertion: (firstArg === secondArg),
-        errorOperator: (" to equal "),
-        secondArg: secondArg
-      });
-    };
+    function _executionContext(context, assertion) {
+      return (context === ' not') ? !assertion : assertion;
+    }
 
-    function toNotEqual (secondArg) {
-      executeTest({
-        assertion: (firstArg !== secondArg),
-        errorOperator: (" not to equal "),
+    function toEqual(secondArg) {
+      _executeTest({
+        assertion: _executionContext(this, (firstArg === secondArg)),
+        errorOperator: (this + " to equal "),
         secondArg: secondArg
       });
     };
 
     function toInclude (secondArg) {
-      executeTest({
-        assertion: (firstArg.includes(secondArg)),
-        errorOperator: (" to contain "),
+      _executeTest({
+        assertion: _executionContext(this, (firstArg.includes(secondArg))),
+        errorOperator: (this + " to contain "),
         secondArg: secondArg
       });
     };
 
+    function instanceOf (secondArg) {
+      _executeTest({
+        assertion: _executionContext(this, (firstArg instanceof secondArg)),
+        errorOperator: (this + " to be an instance of "),
+        secondArg: secondArg
+      })
+    };
+
     function truthy () {
-      executeTest({
+      _executeTest({
         assertion: (!firstArg),
         errorOperator: (" is not true"),
         errorType: 'single comparison'
       });
     };
-    
-    function instanceOf (secondArg) {
-      executeTest({
-        assertion: (firstArg instanceof secondArg),
-        errorOperator: (" to be an instance of "),
-        secondArg: secondArg
-      })
+
+    function falsy () {
+      _executeTest({
+        assertion: (firstArg),
+        errorOperator: (" is not false"),
+        errorType: 'single comparison'
+      });
     };
     
     return {
       toEqual: toEqual,
-      toNotEqual: toNotEqual,
       toInclude: toInclude,
-      toBe: { true: truthy, instanceOf: instanceOf }
+      toBe: {
+        true: truthy,
+        false: falsy,
+        instanceOf: instanceOf
+      },
+      not: {
+        toEqual: toEqual.bind(not),
+        toInclude: toInclude.bind(not),
+        toBe: {
+          instanceOf: instanceOf.bind(not)
+        }
+      }
     }
 
   };
