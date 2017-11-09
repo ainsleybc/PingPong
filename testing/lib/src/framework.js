73ade7a66c559abe097746fab446a8a50bed6271
ainsleybc <ab.chang@me.com>
2017-11-09 21:36:23 +0000

    printing test stats on specRunner.html

--- testing/lib/src/framework.js
+++ testing/lib/src/framework.js
@@ -7,6 +7,7 @@
   var _passedTestCount = 0;
   var _failedTestCount = 0;
   var _container = document.getElementById('spec-runner');
+  var _stats = document.getElementById('test-info');  
   
   exports.output = {
      
@@ -34,6 +35,14 @@
     _renderPage: function () {
       _container.innerHTML += _html;
       _html = '';
+      output._renderStats();
+    },
+
+    _renderStats: function () {
+      var total = document.getElementById('total');
+      var failed = document.getElementById('failed');
+      total.textContent = _passedTestCount + _failedTestCount + ' tests,';
+      failed.textContent = _failedTestCount + ' failures';
     }
 
   };
