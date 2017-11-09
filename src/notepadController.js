a12f5a2621bb4f3a51923a468eb227c3e42b2eb1
Canace Wong <canacewongtoday@hotmail.com>
2017-11-09 13:55:50 +0000

    spike code for displaying entiire note

--- src/notepadController.js
+++ src/notepadController.js
@@ -19,6 +19,27 @@ function NotepadController(containerId) {
     var text = document.getElementById('my-text');
     notepad.addNote(new Note(text.value));
     var navBar = document.getElementById("navBarText");
-    navBar.innerHTML = notepad.listHTML();
+    var notepadview = new NotepadView(notepad);
+    navBar.innerHTML = notepadview.renderFullList();
   };
+
+  NotepadController.prototype.showNote = function () {
+    window.addEventListener("hashchange", showNoteForCurrentPage);
+  };
+
+  NotepadController.prototype.showNoteForCurrentPage = function() {
+  return showNote(getNoteFromUrl(window.location));
+  };
+
+  NotepadController.prototype.getNoteFromUrl = function (location) {
+  return location.hash.split("#")[1];
+  };
+
+  NotepadController.prototype.showNote = function (number) {
+  var notepadview = new NotepadView(notepad);
+  document
+    .getElementById("note-area")
+    .innerText = notepadview.showNote(number);
+  };
+
 }
--- src/notepadView.js
+++ src/notepadView.js
@@ -3,18 +3,25 @@
 (function (exports) {
 
   exports.NotepadView = function (notepad) {
-    
+
     function renderFullList() {
       var html = '<ul>';
       for (var i = 0; i < notepad.notes().length; i++) {
-        html += ('<li>' + notepad.notes()[i].abbreviation() + '</li>');
+        html += ('<a href="#' + i +'"><li>' + notepad.notes()[i].abbreviation() + '</li></a>');
       }
       return html + '</ul>';
     };
 
-    return {
-      renderFullList: renderFullList
+    function showNote(number){
+      return notepad.notes()[number];
     }
+
+    return {
+      renderFullList: renderFullList,
+      showNote: showNote, 
+    }
+
+
 
   }
 
