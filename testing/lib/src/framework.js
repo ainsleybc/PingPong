2e07fb54d569d2ee440505f9fd90bc571c88ce62
ainsleybc <ab.chang@me.com>
2017-11-10 09:24:50 +0000

    refactored test execution into framework

--- testing/lib/src/framework.js
+++ testing/lib/src/framework.js
@@ -8,43 +8,66 @@
   var _failedTestCount = 0;
   var _container = document.getElementById('spec-runner');
   var _stats = document.getElementById('test-info');  
+  var _tests = [];
+
+  function runTests () {
+    _tests.forEach(function (test) {
+      var string = test[0];
+      var callback = test[1];
+      try {
+        callback();
+        addPassingTest(string);
+      }
+      catch (err) {
+        addFailedTest(string, err.stack);
+      };
+    })
+    _tests = [];
+  };
   
+  function renderPage() {
+    _container.innerHTML += _html;
+    _html = '';
+    renderStats();
+  };
+  
+  function renderStats () {
+    var total = document.getElementById('total');
+    var failed = document.getElementById('failed');
+    total.textContent = _passedTestCount + _failedTestCount + ' tests,';
+    failed.textContent = _failedTestCount + ' failures';
+  }
+
+  function startDescribe (text, counter) {
+    _describeCounter++;
+    _html += '<article><p class="describe_block">' + text + '</p>';
+  };
+
+  function endDescribe (counter) {
+    runTests();
+    _describeCounter--;
+    _html += '</article>';
+    if (_describeCounter === 0) renderPage();
+  };
+  
+  function addPassingTest (text) {
+    _html += "<div class='passes'><p>it " + text + "</p></div>";
+    _passedTestCount++;
+  };
+
+  function addFailedTest (text, err) {
+    _html += "<div class='fails'><p>it " + text + "</p><p>" + err + "</p></div>";
+    _failedTestCount++;
+  };
+  
+  function addTest (string, callback) {
+    _tests.push([string, callback]);
+  };
+
   exports.output = {
-     
-    startDescribe: function (text, counter) {
-      _describeCounter++;
-      _html += '<article><p class="describe_block">' + text + '</p>';
-    },
-
-    endDescribe: function (counter) {
-      _describeCounter--;
-      _html += '</article>';
-      if (_describeCounter === 0) output._renderPage();
-    },
-    
-    addPassingTest: function (text) {
-      _html += "<div class='passes'><p>it " + text + "</p></div>";
-      _passedTestCount++;
-    },
-
-    addFailedTest: function (text, err) {
-      _html += "<div class='fails'><p>it " + text + "</p><p>" + err + "</p></div>";
-      _failedTestCount++;
-    },
-
-    _renderPage: function () {
-      _container.innerHTML += _html;
-      _html = '';
-      output._renderStats();
-    },
-
-    _renderStats: function () {
-      var total = document.getElementById('total');
-      var failed = document.getElementById('failed');
-      total.textContent = _passedTestCount + _failedTestCount + ' tests,';
-      failed.textContent = _failedTestCount + ' failures';
-    }
-
+    startDescribe: startDescribe,
+    endDescribe: endDescribe,
+    addTest
   };
 
 })(this);
--- testing/lib/src/it.js
+++ testing/lib/src/it.js
@@ -2,13 +2,7 @@
 
   exports.it = function (string, callback) {
 
-    try {
-      callback();
-      output.addPassingTest(string);
-    }
-    catch (err) {
-      output.addFailedTest(string, err.stack);
-    };
+    output.addTest(string, callback)
 
   };
 
